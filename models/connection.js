67ec31a3d1ad1209db4ca8f20e54da162ab34e68
ramk <ramk@ariztid.com>
2023-06-20 13:57:32 +0530

    second commit

--- models/connection.js
+++ models/connection.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: 'localhost',
+    port:3306,
     user: 'root',
     password:"123456",
     database:"users",
